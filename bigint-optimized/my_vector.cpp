371c67217141e7accb27b9f2cff2c91506b6a985
TanyaCheremkhina <tanyacheremkhina@gmail.com>
2020-09-22 00:35:52 +0300

    fixed

---
+++ bigint-optimized/my_vector.cpp
@@ -0,0 +1,76 @@
+#include "my_vector.h"
+
+#include <iostream>
+#include <cstring>
+#include <cassert>
+#include <algorithm>
+
+my_vector::my_vector() : ref_counter(1) {}
+
+my_vector::my_vector(std::vector<uint32_t> const& vec) : ref_counter(1), vec(vec) {}
+
+my_vector::my_vector(my_vector const& other) : ref_counter(1), vec(other.vec) {}
+
+my_vector::my_vector(uint32_t* src, size_t len) : ref_counter(1) {
+    vec.reserve(len);
+    for (size_t i = 0; i < len; i++) {
+        vec.push_back(src[i]);
+    }
+}
+
+my_vector::my_vector(size_t len, uint32_t x) {
+    vec.resize(len, x);
+    ref_counter = 1;
+}
+
+my_vector& my_vector::operator=(my_vector const& other) {
+    vec = other.vec;
+    ref_counter = 1;
+    return *this;
+}
+
+uint32_t& my_vector::operator[] (size_t i) {
+    return vec[i];
+}
+
+uint32_t const& my_vector::operator[] (size_t i) const {
+    return vec[i];
+}
+
+my_vector* my_vector::get_unique() {
+    if (ref_counter == 1) {
+        return this;
+    }
+    assert(ref_counter > 0);
+    ref_counter--;
+    return new my_vector(vec);
+}
+
+void my_vector::push_back(uint32_t const& x) {
+    vec.push_back(x);
+}
+
+void my_vector::pop_back() {
+    vec.pop_back();
+}
+
+void my_vector::delete_vector() {
+    assert(ref_counter > 0);
+    if (ref_counter == 1) {
+        delete this;
+    } else {
+        ref_counter--;
+    }
+}
+
+void my_vector::inc_ref() {
+    ref_counter++;
+}
+
+bool operator== (my_vector const& a, my_vector const& b) {
+    return a.vec == b.vec;
+}
+
+void my_vector::reverse() {
+    std::reverse(vec.begin(), vec.end());
+}
--- bigint-optimized/my_vector.h
+++ bigint-optimized/my_vector.h
@@ -6,68 +6,25 @@
 #include <cassert>
 
 struct my_vector {
-
-    my_vector() : ref_counter(1) {};
-
-    explicit my_vector(std::vector<uint32_t> const& vec) : vec(vec), ref_counter(1) {};
-
-    my_vector(my_vector const& other) : vec(other.vec), ref_counter(1) {};
-
-    explicit my_vector(size_t const& len, uint32_t const& x = 0) {
-        vec.resize(len, x);
-        ref_counter = 1;
-    }
-
-    my_vector& operator=(my_vector const& other) {
-        ref_counter = 1;
-        vec = other.vec;
-        return *this;
-    }
-
-    uint32_t& operator[] (size_t i) {
-        return vec[i];
-    }
-
-    uint32_t const& operator[] (size_t i) const {
-        return vec[i];
-    }
-
-    my_vector* get_unique() {
-        if (ref_counter == 1) {
-            return this;
-        }
-        assert(ref_counter > 0);
-        ref_counter--;
-        return new my_vector(vec);
-    }
-
-    void push_back(uint32_t const& x) {
-        vec.push_back(x);
-    }
-
-    void pop_back() {
-        vec.pop_back();
-    }
-
-    void delete_vector() {
-        assert(ref_counter);
-        if (ref_counter == 1) {
-            delete this;
-        } else {
-            ref_counter--;
-        }
-    }
-
-    void inc_ref() {
-        ref_counter++;
-    }
-
+    my_vector();
+    explicit my_vector(std::vector<uint32_t> const& vec);
+    my_vector(my_vector const& other);
+    explicit my_vector(size_t len, uint32_t x = 0);
+    my_vector(uint32_t* src, size_t len);
+    my_vector& operator=(my_vector const& other);
+    uint32_t& operator[] (size_t i);
+    uint32_t const& operator[] (size_t i) const;
+    my_vector* get_unique();
+    void push_back(uint32_t const& x);
+    void pop_back();
+    void delete_vector();
+    void inc_ref();
+    void reverse();
     ~my_vector() = default;
-
-    std::vector<uint32_t> vec;
 private:
     size_t ref_counter;
-
+    std::vector<uint32_t> vec;
+    friend bool operator== (my_vector const& a, my_vector const& b);
 };
 
 #endif //BIGINT_MY_VECTOR_H
